f34a26a1d4eb7c675679eb5621cd75cdebd4d2d7
walid amin <walidfamin@gmail.com>
2025-08-03 13:51:36 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CardHeader>
    
    with

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,7 @@ export default function DashboardPage() {
     return {
       totalIncome: income,
       totalExpenses: expenses,
-      netSavings: income - expenses - investmentAmount,
+      netSavings: income - expenses,
       investmentsMade: investmentAmount,
     };
   }, [walidTransactions]);
